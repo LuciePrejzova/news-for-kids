ba939107bdfab3b44f193ac51c6c882611d617d8
LuciePrejzova <lucie.prejzova@gmail.com>
2025-05-10 12:05:12 +0200

    setup newsoftheday

--- src/components/NewsOfTheDay.jsx
+++ src/components/NewsOfTheDay.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './NewsOfTheDay.css'
+import React, { useState, useEffect } from "react";
+import "./NewsOfTheDay.css";
+import { fetchLatestNews } from "../api/news";
 
 const NewsOfTheDay = () => {
-  return (
-    <div>NewsOfTheDay</div>
-  )
-}
+  const [article, setArticle] = useState({});
+  const [loading, setLoading] = useState(true);
 
-export default NewsOfTheDay
+  useEffect(() => {
+    fetchLatestNews()
+      .then((data) => {
+        setArticle(data[0]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching news: " + error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (!article) return <p>No article found</p>;
+
+  return <div>
+    <h2>News Of The Day</h2>
+    <img src={article.image}/>
+    <h3>{article.title}</h3>
+  </div>;
+};
+
+export default NewsOfTheDay;
